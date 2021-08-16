21b0c58acd0ea54bd59d9846d4f83e8a0507bbbe
CW-MD <cdayton96@gmail.com>
2021-08-16 17:29:22 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "portfolio_v2",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
