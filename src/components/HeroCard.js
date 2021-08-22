3156959f68653abcafb32d649bec1cb10f1e28cc
CW-MD <cdayton96@gmail.com>
2021-08-22 18:48:48 -0400

    removed unused comments

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -2,15 +2,6 @@ import * as React from 'react'
 import '../styles/global.css'
 import  Typewriter  from 'typewriter-effect';
 
-// let titles = ['Graphic Designer', 'Web Developer', 'Software Engineer'];
-
-// let target = document.getElementById('type-sec')
-
-// var typewriter = TypewriterClass(target,{loop:true})
-
-// for(let elem in titles){
-//     typewriter.pauseFor(2500).typeString(elem).pauseFor(500).deleteChars(elem.length)
-// }
 
 const HeroCard = () => {
     return (
@@ -27,10 +18,6 @@ const HeroCard = () => {
                 
         }}/>
 
-         
-            {/* <div style={{marginLeft:'20%', marginTop:'-5%',lineHeight:'5rem',  }}>Graphic Designer</div>
-            <div style={{marginLeft:'30%', lineHeight:'5rem'}}>Web Developer</div>
-            <div style={{marginLeft:'40%', paddingBottom:"100px",lineHeight:'5rem'}}>Software Engineer</div> */}
 
         </div>
 
